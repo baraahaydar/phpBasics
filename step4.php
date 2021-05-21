ecc3fd8880e5efe1b132fe1302eaabc0387a4701
baraa haydar <baraa97.bh@gmail.com>
2021-05-21 14:05:34 +0300

    step4

---
+++ step4.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'Baraa';
+$position = 'Tripoli';
+$github_url = 'https://github.com/baraahaydar';
+$me = 'Otional: Hello, My name is '.$name.', From '.$position.', You can find the github '.$github_url; 
+echo "Hello, My name is $name, "."From $position,"." You can find the github $github_url";
+echo "<br/>";
+echo "Hello, My name is $name, From $position, You can find the github $github_url";
+echo "<br/>";
+echo 'Hello, My name is '.$name.', From '.$position.', You can find the github '.$github_url;
+echo "<hr/>";
+echo $me;
+echo "<hr/>";
+?>
