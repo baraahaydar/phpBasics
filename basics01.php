98c67c14ffac03f60fcb650c6aa5426e5d51f091
baraa haydar <baraa97.bh@gmail.com>
2021-05-21 11:40:39 +0300

    step2

--- basics01.php
+++ basics01.php
@@ -1,3 +1,9 @@
 Hello world
 <?php
+echo "<br/>";
+echo "hello world";
+echo "<br/>";
+echo "I am getting errors but I can't see the errors";
+echo "<br/>";
+print "Search on how to enable PHP Errors";
 ?>
