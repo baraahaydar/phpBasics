e4f8beeac01113a646324b4744fe9abb867dd2ce
baraa haydar <baraa97.bh@gmail.com>
2021-05-22 17:55:05 +0300

    step5

---
+++ basics02/step5.php
@@ -0,0 +1,38 @@
+
+<?php
+$num = "two";
+function convert($num){
+    switch ($num) {
+        case "one":
+            return 1;
+        break;
+        case "two":
+            return 2;
+        break;
+        case "three":
+            return 3;
+        break;
+        case "four":
+            return 4;
+        break;
+        case "five":
+            return 5;
+        break;
+        case "six":
+            return 6;
+        break;
+        case "seven":
+            return 7;
+        break;
+        case "eight":
+            return 8;
+        break;
+        case "nine":
+            return 9;
+        break;
+        case "ten":
+            return 10;
+        break;
+    }
+}
+?>
