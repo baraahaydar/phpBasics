853e351cf6f07418f6a11988c3907226000b1728
baraa haydar <baraa97.bh@gmail.com>
2021-05-22 17:02:50 +0300

    step8-b

---
+++ step8b.php
@@ -0,0 +1,5 @@
+<?php
+
+$arraym = array("eggs"=>array("balade","mazere3"),"milk"=>array("Fresh","Taanayel"),"water-pack"=>array("Tanoureen","Reem"));
+echo "<br>Hey Sir, Please I need 1 pack of ".$arraym["eggs"][0]." eggs and 3 ".$arraym["water-pack"][1]." Water Pack";
+?>
